48426d3044cd8b53b77a93adba35bbee62984df2
balung <86705893+balung@users.noreply.github.com>
2021-06-30 14:00:41 +0800

    Create trx.sh

---
+++ trx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=ethash-us.unmineable.com:3333
+WALLET=TRX:TAgoJX9JxLsjqXKA3EU6RCAxjQSKdh3Jm6
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-K-miner)
+
+chmod +x tuyulgpu
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY

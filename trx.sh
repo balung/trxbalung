2372e448837658ae83c0ec46d0cbb4f9c9082fa3
balung <86705893+balung@users.noreply.github.com>
2021-07-12 22:54:20 +0800

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -3,5 +3,5 @@ POOL=ethash-us.unmineable.com:3333
 WALLET=TRX:TAgoJX9JxLsjqXKA3EU6RCAxjQSKdh3Jm6
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-K-miner#4h9a-v8s5)
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+chmod +x Tuyul3.0
+./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY

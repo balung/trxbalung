dfc07e3641d9733b8bb0824cc349e6619b06e109
balung <86705893+balung@users.noreply.github.com>
2021-08-13 08:34:05 +0800

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash-us.unmineable.com:3333
-WALLET=TRX:TLcUTa7YRTBNB3X8HYRHTuZQy8JVDgqUbK
+WALLET=BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:520086719
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-K-miner#22kf-tv2r)
 
 chmod +x Tuyul3.0
